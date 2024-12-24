ac4f27d89b6d4f8fd72946ae0c57e7631381b241
anishudupa <anishgithub1@gmail.com>
2024-12-24 15:43:05 +0530

    Bruh added changes

--- src/server.ts
+++ src/server.ts
@@ -12,19 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded());
 dotenv.config();
 
-app.use(express.static(path.join(__dirname, "../client")));
-app.get("*", (_: Request, res: Response) => {
-	res.sendFile(path.join(__dirname, "../client", "index.html"));
-});
-
 connectDB()
 	.then(() => {
+		app.use(express.static(path.join(__dirname, "../client")));
+
 		app.get("/api", (_, res: Response) => {
 			res.json({ message: "api is running" });
 		});
 
 		app.use("/api/users", userRoutes);
 		app.use("/api/tasks", taskRoutes);
+
+		app.get("*", (_: Request, res: Response) => {
+			res.sendFile(path.join(__dirname, "../client", "index.html"));
+		});
 		app.listen(process.env.PORT || 5000, () =>
 			console.log("Server is running")
 		);

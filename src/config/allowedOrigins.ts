f9cc268fa560230c05f8f332814c95d90670fba4
anishudupa <anishgithub1@gmail.com>
2025-01-09 19:05:56 +0530

    Changes to CORS

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrdigins = [
 	"http://localhost:5000",
 	"http://localhost:5173",
-	"https://taskify-ten-mu.vercel.app/",
+	"https://taskify-ten-mu.vercel.app",
 ];
 
 export default allowedOrdigins;
